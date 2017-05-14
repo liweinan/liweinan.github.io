23840b31ff29fa3d5ffe84cb4fc57df8be510764
Weinan <l.weinan@gmail.com>
2017-05-14 04:53:22 +0000

    .

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-JEKYLL_ENV=production jekyll build
-git add assets/* CNAME _config.yml Gemfile _includes index.md _layouts _posts README.md _site push.sh feed.xml .drafts/*
+#JEKYLL_ENV=production jekyll build
+git add CNAME _config.yml Gemfile _includes index.md _layouts _posts README.md _site push.sh feed.xml .drafts/*
 git commit -a -m '.'
 git push origin master
 
