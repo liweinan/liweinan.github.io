4a96cde981099d3f3d71c0ebceccb0d9d0fe4049
Weinan <l.weinan@gmail.com>
2017-05-29 11:27:24 +0000

    .

--- _site/push.sh
+++
@@ -1,5 +0,0 @@
-JEKYLL_ENV=production jekyll build
-git add CNAME _config.yml Gemfile _includes index.md _layouts _posts README.md _site push.sh feed.xml .drafts/*
-git commit -a -m '.'
-git push origin master
-
