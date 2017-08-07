15cbc0162bd9f0af177519fa75854636e598beb6
Weinan Li <l.weinan@gmail.com>
2017-08-07 15:54:48 +0800

    .

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-git add assets/* CNAME _config.yml Gemfile _includes index.md _layouts _posts README.md _site push.sh
+git add assets/* CNAME _config.yml Gemfile _includes index.md _posts README.md _site push.sh
 git commit -a -m '.'
 git push origin master
 
