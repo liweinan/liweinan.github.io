a35372b0d5c1a28279aff0b9b2644002b10b9383
Weinan Li <l.weinan@gmail.com>
2020-01-31 23:51:45 +0800

    .

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ set -x
 
 git fetch origin
 git rebase origin/master
-git add CNAME _config.yml Gemfile _includes index.html _posts README.md push.sh
+git add CNAME _config.yml _includes index.html _posts README.md push.sh
 git commit -a -m '.'
 git push origin master
 
